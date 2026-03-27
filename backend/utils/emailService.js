07f25003c32622fda6575b19e18dd0f56b6cb3fd
OmiJ80 <omijadhav6080@gmail.com>
2026-03-27 20:09:42 +0530

    forgot pass

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,10 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE || 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, 
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS || process.env.EMAIL1_PASS,
+        pass: process.env.EMAIL_PASS,
     },
 });
 
